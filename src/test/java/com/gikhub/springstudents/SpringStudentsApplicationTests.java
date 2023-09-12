b21e9b5b733b47944660aa0759e111589147cabc
gik-hub <admin@Admins-MacBook-Pro.local>
2023-09-12 21:40:32 +0300

    initialising spring project

---
+++ src/test/java/com/gikhub/springstudents/SpringStudentsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gikhub.springstudents;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringStudentsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
